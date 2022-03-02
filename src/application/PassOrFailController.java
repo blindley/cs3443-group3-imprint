fe553ade05879fa64c52efec48344ba9befa2cc9
Benjamin Lindley <benjameslindley@gmail.com>
2022-03-02 00:02:30 -0600

    now loading deck from external file

--- src/application/PassOrFailController.java
+++ src/application/PassOrFailController.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
@@ -19,17 +21,33 @@ public class PassOrFailController implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		session = new ReviewSession();
 		
-		String[] sampleCardData = {
-			"What is the capital of France?", "Paris",
-			"What is the derivative of sin(x)?", "cos(x)",
-			"What is the opposite of left?", "right",
-			"What is the average distance from the Earth to the Sun", "93 million miles",
-			"How many letters are in the alphabet?", "26"
-		};
+		ArrayList<String[]> cardData = new ArrayList<String[]>();
 		
-		for (int i = 0; i < sampleCardData.length / 2; i++) {
-			String front = sampleCardData[i * 2];
-			String back = sampleCardData[i * 2 + 1];
+		try {
+			cardData = CSVLoader.loadCSV("state-capitals.csv");
+		} catch (IOException e) {
+			String[] sampleCardData = {
+				"What is the capital of France?", "Paris",
+				"What is the derivative of sin(x)?", "cos(x)",
+				"What is the opposite of left?", "right",
+				"What is the average distance from the Earth to the Sun", "93 million miles",
+				"How many letters are in the alphabet?", "26"
+			};
+
+			cardData = new ArrayList<String[]>();
+
+			for (int i = 0; i < sampleCardData.length / 2; i++) {
+				String[] row = new String[3];
+				row[0] = "" + i;
+				row[1] = sampleCardData[i * 2];
+				row[2] = sampleCardData[i * 2 + 1];
+				cardData.add(row);
+			}
+		}
+		
+		for (int i = 0; i < cardData.size(); i++) {
+			String front = cardData.get(i)[1];
+			String back = cardData.get(i)[2];
 			
 			FlashCard fc = new FlashCard();
 			fc.setFront(front);
