54b8700fd940f54d1c75f20fca582efed2b0ad94
Benjamin Lindley <benjameslindley@gmail.com>
2022-03-07 07:45:01 -0600

    loading (but not using) config file

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,9 @@
 package application;
 	
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
@@ -30,6 +34,23 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		String configFilePath = "config.csv";
+		File configFile = new File(configFilePath);
+		if (configFile.exists() && configFile.isFile()) {
+			try {
+				ArrayList<String[]> configData = CSVLoader.loadCSV(configFilePath);
+				for (String[] row : configData) {
+					System.out.println(String.join(", ", row));
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} else {
+			System.out.println(configFilePath + " does not exist");
+		}
+		
 		launch(args);
 	}
 	
