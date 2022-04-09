e524702d590b74807ae11aa3878932c88dabd682
Benjamin Lindley <benjameslindley@gmail.com>
2022-04-09 10:14:36 -0500

    wrap text on label

--- src/application/DeckSelectionController.java
+++ src/application/DeckSelectionController.java
@@ -45,6 +45,7 @@ public class DeckSelectionController implements Initializable {
 		
 		deckListView.setItems(deckNameList);
 		deckInfoLabel.setText("");
+		deckInfoLabel.setWrapText(true);
 	}
 	
 	@FXML
