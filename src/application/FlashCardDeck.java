37e03d057699a18464616109b108f724bd4f61f4
jcslicker23 <99768787+jcslicker23@users.noreply.github.com>
2022-04-24 11:02:28 -0500

    Update FlashCardDeck.java
    Updates

--- src/application/FlashCardDeck.java
+++ src/application/FlashCardDeck.java
@@ -9,15 +9,23 @@ public class FlashCardDeck {
 	FlashCardDeck() {
 		theCards = new HashMap<String, FlashCard>();
 	}
-
+	/**
+	 * @param id
+	 * @return
+	 */
 	public FlashCard getCard(String id) {
 		return theCards.get(id);
-	}
-
+	}	
+	/**
+	 * @param id
+	 * @param card
+	 */
 	public void addOrUpdateCard(String id, FlashCard card) {
 		theCards.put(id, card);
 	}
-
+	/**
+	 * @return
+	 */
 	public TreeSet<String> getCardIds() {
 		TreeSet<String> ids = new TreeSet<String>();
 		for (String id : theCards.keySet()) {
